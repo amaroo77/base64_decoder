964bf4d3505ab33ad5d8107f099e2b295ff870ea
amaroo77 <73893037+amaroo77@users.noreply.github.com>
2020-12-14 12:43:29 +0800

    Add files via upload

---
+++ base64_decoder.py
@@ -0,0 +1,10 @@
+import base64
+print('')
+print('This is Base64 decode')
+print('')
+print('')
+code = input("What's the code?"
+             ,)
+print('')
+print("This is the results ===>  ", base64.b64decode(code))
+quit()
